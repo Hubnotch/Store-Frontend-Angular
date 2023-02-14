0e1f7caf54376c7f783f493095cb3a65213d57a4
Ugwuanyi Love Ekene <49119675+Hubnotch@users.noreply.github.com>
2023-02-14 10:38:52 +0100

    Updated product component

--- src/app/component/product/product.component.ts
+++ src/app/component/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from 'src/app/model/Product';
+import { CartService } from 'src/app/services/cart.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -7,20 +9,32 @@ import { Product } from 'src/app/model/Product';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
-  @Input() product: Product;
-  @Output() addToCart: EventEmitter<Product> = new EventEmitter();
+  @Input() product!: Product;
+  selectedItem = '1';
+  productCount: string[] = ['1', '2', '3', '4', '5'];
 
-  constructor() {
-    this.product = {
-      id: 0,
-      name: '',
-      price: 0,
-      url: '',
-      description: ''
+
+  constructor(private productService:ProductService, private cartService:CartService) {}
+  ngOnInit():void {}
+  selectedChange(value: any): void { 
+    this.selectedItem = value;
+  }
+
+  addProductToCart(product: Product): void {
+    const cartProducts: Product[] = this.cartService.getCartProduct();
+    let productInCart = cartProducts.find(product => product.id === product.id);
+    if (productInCart) {
+      productInCart.amount = this.selectedItem;
+      productInCart ? this.productService.addProduct(cartProducts) : null;
+    } else {
+      cartProducts.push(Object.assign(product, { amount: this.selectedItem }));
+      this.productService.addProduct(cartProducts);
+      const message = `${product.name} has been added to your cart.`;
+      alert(message);
     }
-  }
-  addItemToCart(product:Product): void {
-    this.addToCart.emit(product);
-  }
- 
+    this.refresh();
+}
+  refresh(): void {
+    window.location.reload();
+}
 }
